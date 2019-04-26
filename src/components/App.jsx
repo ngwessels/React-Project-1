f9bc93468e074e2c833cb97b53f4032be2d8092a
Nate Wessels <nwessels16@gmail.com>
2019-04-26 10:36:18 -0700

    change APP.jsx function to class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './navBar/Header';
 import { Switch, Route } from 'react-router-dom';
 import Error404 from './Error404';
 import Page from './Page/PageBody';
 import FooterMain from './Footer/FooterMain';
-function App(){
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={Page}/>
-        <Route component={Error404}/>
-      </Switch>
-      <FooterMain />
-    </div>
-  );
+
+
+
+
+class App extends React.Component{
+  constructor(props) {
+    super(props)
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={Page}/>
+          <Route component={Error404}/>
+        </Switch>
+        <FooterMain />
+      </div>
+    );
+  }
+
 }
 
 export default App;
